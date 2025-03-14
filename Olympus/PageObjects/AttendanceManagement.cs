bde646df53de534ed616a27d3ba08c7841bec1c4
Manoj885198369 <manojtyson4@gmail.com>
2025-03-14 17:12:44 +0530

    g

---
+++ Olympus/PageObjects/AttendanceManagement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Olympus.PageObjects
+{
+    class AttendanceManagement
+    {
+        public AttendanceManagement(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How = How.XPath, Using = "//span[text()='Time Management']")]
+        private IWebElement TimeManagement;
+        [FindsBy(How = How.XPath, Using = "//a[@href='https://icehrmpro.gamonoid.com/?g=modules&n=attendance&m=module_Time_Management']")]
+        private IWebElement attendance;
+        [FindsBy(How = How.XPath, Using = "//span[text()='Punch In']")]
+        private IWebElement punchin;
+        [FindsBy(How = How.XPath, Using = "//div[@class='ant-picker ant-picker-middle ant-picker-outlined css-dev-only-do-not-override-240cud']")]
+        private IWebElement Time;
+        [FindsBy(How = How.XPath, Using = "Save")]
+        private IWebElement savebutton;
+
+        public IWebElement TimeManagement1 { get => TimeManagement; set => TimeManagement = value; }
+        public IWebElement Attendance { get => attendance; set => attendance = value; }
+        public IWebElement Punchin { get => punchin; set => punchin = value; }
+        public IWebElement Time1 { get => Time; set => Time = value; }
+        public IWebElement Savebutton { get => savebutton; set => savebutton = value; }
+    }
+}
---
+++ Olympus/PageObjects/LeaveManagement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Olympus.PageObjects
+{
+    class LeaveManagement
+    {
+        public LeaveManagement(IWebDriver driver)
+        {
+            PageFactory.InitElements(driver, this);
+        }
+        [FindsBy(How=How.XPath,Using = "//span[text()='Leave']")]
+        private IWebElement leavebutton;
+             [FindsBy(How = How.XPath, Using = "//a[@href='https://icehrmpro.gamonoid.com/?g=modules&n=leaves&m=module_Leave']")]
+        private IWebElement leavemanagement;
+             [FindsBy(How = How.XPath, Using = "//button[text()='Apply Leave ']")]
+        private IWebElement applyleave;
+             [FindsBy(How = How.XPath, Using = "//select[@id='leave_type']")]
+        private IWebElement leavetype;
+             [FindsBy(How = How.XPath, Using = "//input[@id='date_start']")]
+        private IWebElement leavestartdate;
+             [FindsBy(How = How.XPath, Using = "//input[@id='date_start']")]
+        private IWebElement leaveenddate;
+             [FindsBy(How = How.XPath, Using = "//textarea[@id='details']")]
+        private IWebElement reason;
+             [FindsBy(How = How.XPath, Using = "//button[text()='Continue']")]
+        private IWebElement Continue;
+
+        public IWebElement Leavebutton { get => leavebutton; set => leavebutton = value; }
+        public IWebElement Leavemanagement { get => leavemanagement; set => leavemanagement = value; }
+        public IWebElement Applyleave { get => applyleave; set => applyleave = value; }
+        public IWebElement Leavetype { get => leavetype; set => leavetype = value; }
+        public IWebElement Leavestartdate { get => leavestartdate; set => leavestartdate = value; }
+        public IWebElement Leaveenddate { get => leaveenddate; set => leaveenddate = value; }
+        public IWebElement Reason { get => reason; set => reason = value; }
+        public IWebElement Continue1 { get => Continue; set => Continue = value; }
+    }
+}
